f2a2e5d9b210f1f40e8c5bb33e2a507b897f2ce4
roshanstha11 <roshan.shrestha@ku.edu.np>
2025-09-17 20:35:53 +0545

    Little design changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="{{ asset('images/kulogo.png') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,19 +5,40 @@
         <a class="navbar-brand" href="/">
             <img src="{{ asset('images/kulogo.png') }}" alt="KU Logo" height="80" class="ps-5">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarNav">
+        @auth
+        <div class="col-auto">
+            <div class="dropdown">
+                <button class="btn btn-link text-white text-decoration-none d-flex align-items-center gap-2 p-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div class="bg-secondary rounded-circle d-flex align-items-center justify-content-center" style="width: 35px; height: 35px;">
+                        <i class="bi bi-person-fill text-white"></i>
+                    </div>
+                    <span class="fw-medium">{{ Auth::user()->email }}</span>
+                    <i class="bi bi-chevron-down"></i>
+                </button>
+                
+                <ul class="dropdown-menu dropdown-menu-end shadow-lg border-0 rounded-3 p-3" style="min-width: 280px;">
+                    <li class="dropdown-header px-0 pb-3 mb-3 border-bottom">
+                        <div class="fw-bold text-dark mb-2">Name: {{ Auth::user()->name }}</div>
+                        <div class="badge bg-light text-dark rounded-pill">Role: {{ Auth::user()->role }}</div>
+                    </li>
+                    
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            {{-- <button type="submit" class="dropdown-item">Logout</button> --}}
+                            <button type="submit" class="dropdown-item d-flex align-items-center gap-3 py-2 px-3 rounded-2 text-danger" onclick="return confirm('Are you sure you want Logout?');">
+                                <i class="bi bi-box-arrow-right"></i>
+                                Logout
+                            </button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        @endauth
+        {{-- <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    {{-- If the user is not logged in --}}
-                    {{-- @guest
-                    <a type="button" class="btn btn-info" href="{{ route('login') }}">Log In</a>
-                    <a type="button" class="btn btn-info" href="{{ route('register') }}">Register</a>
-                    @endguest --}}
-                    {{-- If the user is logged in --}}
                     @auth
                     <div class="btn-group">
                         <button class="btn btn-info dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
@@ -38,7 +59,7 @@
                     @endauth
                 </li>
             </ul>
-        </div>
+        </div> --}}
     </div>
 </nav>
 
